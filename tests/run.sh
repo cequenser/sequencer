fe279018922006e8df8d6eab562f85d370f6eff7
Lars <lars.lubkoll@posteo.de>
2019-07-12 14:48:09 +0200

    Enable line and branch coverage
    Change-Id: I98f8942083adad34237da1dc906d50c981fd7b1c

--- tests/run.sh
+++ tests/run.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+COMPILER=$1
+COMPUTE_COVERAGE=$2
+GCOV=$3
+
 cd /home/shared
 mkdir -p build
 cd build
 rm -rf *
 
-cmake .. -DBuildTests=ON -DCMAKE_CXX_COMPILER=$1
+cmake .. -DBuildTests=ON -DCMAKE_CXX_COMPILER=${COMPILER}
 cmake --build .
 
 tests/tests
+
+if [ "${COMPUTE_COVERAGE}" = "true" ]; then
+      lcov --gcov-tool ${GCOV} --capture --no-external --directory . --base-directory ../include -rc lcov_branch_coverage=1 --output-file coverage.info
+      coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
+fi

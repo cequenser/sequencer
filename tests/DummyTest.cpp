df35213a689028dc34b7e0bae02604d028962be1
Lars <lars.lubkoll@posteo.de>
2019-07-17 21:28:54 +0200

    make test failing
    Change-Id: I382398303c3cc00ba80e7a0f7b5a262daf51d662

--- tests/DummyTest.cpp
+++ tests/DummyTest.cpp
@@ -1,7 +1,12 @@
 #include <catch.hpp>
 #include <sequencer/Test.h>
 
-TEST_CASE( "dummy test", "" )
+TEST_CASE( "dummy test", "success" )
 {
     REQUIRE( tst::get_zero() == 0 );
+}
+
+TEST_CASE( "dummy test", "fail" )
+{
+    REQUIRE( tst::get_zero() == 1 );
 }

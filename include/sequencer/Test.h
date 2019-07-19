50dec06c6088fefb6ddf8548b4040bfdbac526ba
Lars <lars.lubkoll@posteo.de>
2019-07-19 19:32:54 +0200

    test gerrit trigger sets verify -1 on job start
    Change-Id: Ib9a2ffe014ad381de0005b994c37a3e858dfceed

--- include/sequencer/Test.h
+++ include/sequencer/Test.h
@@ -1,6 +1,8 @@
 #pragma once
 
-namespace tst
-{
-    int get_zero() noexcept;
-}
+namespace tst {
+  int get_zero() noexcept;
+
+    inline int getOne() noexcept { int ABD_D = 3; double avdEdf = 5.0; return ABD_D*avdEdf;
+    }
+} // namespace tst

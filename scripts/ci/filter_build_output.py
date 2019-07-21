d868f9f7f08898b9ea0c7495741bf766bde7c335
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-07-21 18:50:23 +0200

    Add script to reduce information from build-output that is reported to gerrit
    Change-Id: Id4f356d04683709b5ad63a0064ff4167ca6045aa

---
+++ scripts/ci/filter_build_output.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os
+import re
+import sys
+
+def filter_build_output(build_log_name):
+    build_log = open(build_log_name, 'r')
+    filtered_output = ""
+    for line in build_log:
+        m = re.match("\[[0-9]+/[0-9]+\] ", line)
+        if m:
+            continue
+        m = re.match("ninja: build stopped:", line)
+        if m:
+            continue
+        filtered_output = filtered_output + line
+    print filtered_output
+
+
+if len(sys.argv) == 2:
+    filter_build_output(sys.argv[1])

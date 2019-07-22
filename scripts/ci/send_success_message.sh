65cfb293be6f81ea435ba13d8104a39debce2e49
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-07-22 16:05:16 +0200

    Add script for sending success messages for custom labels
    Change-Id: I7a4053e4e8877f6ea4ff2599ee53777790d6ed12

---
+++ scripts/ci/send_success_message.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LABEL=$1
+BUILD_URL=$2
+PATCHSET_REVISION=$3
+
+sed 's/<Label>/${LABEL}/g' scripts/ci/message_only_template.json | \
+sed 's/<VERIFIED>/+1/g' | \
+sed 's@<MESSAGE>@${BUILD_URL}: SUCCESS@g' | \
+ssh -p 29418 cequenser-ci@review.gerrithub.io gerrit review ${PATCHSET_REVISION} --json

6216564aed41d23c49d3ce2714d2af1fb07ea1ea
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-07-22 16:18:49 +0200

    Fix invocation of sed
    Change-Id: I1d8fd2d7dce59e86046b2b53b9a496b290863245

--- scripts/ci/send_success_message.sh
+++ scripts/ci/send_success_message.sh
@@ -4,7 +4,7 @@ LABEL=$1
 BUILD_URL=$2
 PATCHSET_REVISION=$3
 
-sed 's/<Label>/${LABEL}/g' scripts/ci/message_only_template.json | \
-sed 's/<VERIFIED>/+1/g' | \
-sed 's@<MESSAGE>@${BUILD_URL}: SUCCESS@g' | \
+sed "s/<Label>/${LABEL}/g" scripts/ci/message_only_template.json | \
+sed "s/<VERIFIED>/+1/g" | \
+sed "s@<MESSAGE>@Build Successful ${BUILD_URL}@g" | \
 ssh -p 29418 cequenser-ci@review.gerrithub.io gerrit review ${PATCHSET_REVISION} --json

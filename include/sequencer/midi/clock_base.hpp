c137516af4b2fcde52ad3138854d5782c0cdfe18
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-07 20:09:33 +0200

    Add midi clock
    Change-Id: I6d8d1497ae73889e9f1ba9b0672dd9cb43d869fa

---
+++ include/sequencer/midi/clock_base.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <sequencer/beat_time_point.hpp>
+#include <sequencer/midi/message_type.hpp>
+
+#include <cassert>
+
+namespace sequencer::midi
+{
+    class clock_base
+    {
+    public:
+        constexpr clock_base() = default;
+
+        constexpr explicit clock_base( beat_time_point start_time ) noexcept
+            : last_update_( start_time ), start_time_( start_time )
+        {
+        }
+
+        template < typename Sender >
+        void update( beat_time_point t, Sender sender )
+        {
+            if ( !running_ && started_ )
+            {
+                sender( continue_ ? midi::message_type::realtime_continue
+                                  : midi::message_type::realtime_start );
+                running_ = true;
+                continue_ = true;
+            }
+            else if ( running_ && !started_ )
+            {
+                sender( midi::message_type::realtime_stop );
+                running_ = false;
+            }
+
+            if ( running_ )
+            {
+                const auto dt = tick();
+                while ( last_update_ + dt <= t )
+                {
+                    last_update_ += dt;
+                    sender( midi::message_type::realtime_clock );
+                }
+            }
+        }
+
+        constexpr bool is_started() const noexcept
+        {
+            return started_;
+        }
+
+        void reset() noexcept
+        {
+            started_ = false;
+            continue_ = false;
+            last_update_ = start_time_;
+        }
+
+        constexpr void start() noexcept
+        {
+            started_ = true;
+        }
+
+        constexpr void stop() noexcept
+        {
+            started_ = false;
+        }
+
+        constexpr void set_pulses_per_quarter_note( int pulses_per_quarter_note ) noexcept
+        {
+            assert( pulses_per_quarter_note > 0 );
+            pulses_per_quarter_note_ = pulses_per_quarter_note;
+        }
+
+        constexpr int pulses_per_quarter_note() const noexcept
+        {
+            return pulses_per_quarter_note_;
+        }
+
+    private:
+        constexpr beat_duration tick() const noexcept
+        {
+            return beat_duration( 1.0 / pulses_per_quarter_note() );
+        }
+
+        beat_time_point last_update_;
+        beat_time_point start_time_;
+        int pulses_per_quarter_note_{24};
+        bool started_ = false;
+        bool running_ = false;
+        bool continue_ = false;
+    };
+
+} // namespace sequencer::midi
---
+++ tests/sequencer/midi/clock_base_test.cpp
@@ -0,0 +1,282 @@
+#include <sequencer/midi/clock_base.hpp>
+
+#include <catch2/catch.hpp>
+
+SCENARIO( "start and stop messages", "[midi_clock_base]" )
+{
+    using namespace sequencer;
+
+    GIVEN( "A midi_clock" )
+    {
+        midi::clock_base clock;
+
+        WHEN( "the clock is not started" )
+        {
+            THEN( "nothing happens when update is called" )
+            {
+                clock.update( beat_time_point( 1.0_beats ), []( midi::message_type ) { throw 1; } );
+            }
+            THEN( "is_started returns false" )
+            {
+                REQUIRE_FALSE( clock.is_started() );
+            }
+            THEN( "is_started still returns false after stop" )
+            {
+                clock.stop();
+                REQUIRE_FALSE( clock.is_started() );
+
+                THEN( "nothing happens when update is called" )
+                {
+                    clock.update( beat_time_point( 1.0_beats ),
+                                  []( midi::message_type ) { throw 1; } );
+                }
+            }
+            THEN( "is_started still returns false after reset" )
+            {
+                clock.reset();
+                REQUIRE_FALSE( clock.is_started() );
+
+                THEN( "nothing happens when update is called" )
+                {
+                    clock.update( beat_time_point( 1.0_beats ),
+                                  []( midi::message_type ) { throw 1; } );
+                }
+            }
+        }
+
+        WHEN( "the clock is started" )
+        {
+            clock.start();
+            THEN( "a start message is sent when update is called" )
+            {
+                midi::message_type message = midi::message_type::invalid;
+                clock.update( beat_time_point( 0.0_beats ),
+                              [& msg = message]( midi::message_type message ) { msg = message; } );
+                REQUIRE( message == midi::message_type::realtime_start );
+
+                THEN( "nothing happens when update is called again" )
+                {
+                    clock.update( beat_time_point( 0.0_beats ),
+                                  []( midi::message_type ) { throw 1; } );
+                }
+
+                THEN( "is_started returns false after stop" )
+                {
+                    clock.stop();
+                    REQUIRE_FALSE( clock.is_started() );
+
+                    THEN( "a stop message is sent when update is called again" )
+                    {
+                        midi::message_type received = midi::message_type::invalid;
+                        clock.update( beat_time_point( 1.0_beats ),
+                                      [&received]( midi::message_type message ) {
+                                          REQUIRE( received == midi::message_type::invalid );
+                                          received = message;
+                                      } );
+                        REQUIRE( received == midi::message_type::realtime_stop );
+
+                        THEN( "nothing happens when update is called again" )
+                        {
+                            clock.update( beat_time_point( 1.0_beats ),
+                                          []( midi::message_type ) { throw 1; } );
+                        }
+                    }
+                }
+
+                THEN( "is_started returns false after reset" )
+                {
+                    clock.reset();
+                    REQUIRE_FALSE( clock.is_started() );
+
+                    midi::message_type received = midi::message_type::invalid;
+                    clock.update( beat_time_point( 1.0_beats ),
+                                  [&received]( midi::message_type message ) {
+                                      REQUIRE( received == midi::message_type::invalid );
+                                      received = message;
+                                  } );
+                    REQUIRE( received == midi::message_type::realtime_stop );
+
+                    THEN( "nothing happens when update is called again" )
+                    {
+                        clock.update( beat_time_point( 1.0_beats ),
+                                      []( midi::message_type ) { throw 1; } );
+                    }
+                }
+            }
+            THEN( "is_started returns true" )
+            {
+                REQUIRE( clock.is_started() );
+            }
+        }
+    }
+
+    GIVEN( "A midi clock that starts at -4.0 beats" )
+    {
+        midi::clock_base clock( beat_time_point( -4.0_beats ) );
+        CHECK( clock.pulses_per_quarter_note() == 24 );
+        clock.start();
+
+        WHEN( "update is called at 1.0 beat " )
+        {
+            THEN( "the clock sends 5 x 24 pulses" )
+            {
+                int pulse_count = 0;
+                clock.update( beat_time_point( 1.0_beats ),
+                              [&pulse_count]( midi::message_type message ) {
+                                  if ( message == midi::message_type::realtime_clock )
+                                  {
+                                      pulse_count++;
+                                  }
+                              } );
+                REQUIRE( pulse_count == 5 * clock.pulses_per_quarter_note() );
+
+                WHEN( "update is called at 1.0 beat after reset" )
+                {
+                    clock.reset();
+                    clock.start();
+                    THEN( "the clock sends 5 x 24 pulses again" )
+                    {
+                        int pulse_count = 0;
+                        clock.update( beat_time_point( 1.0_beats ),
+                                      [&pulse_count]( midi::message_type message ) {
+                                          if ( message == midi::message_type::realtime_clock )
+                                          {
+                                              pulse_count++;
+                                          }
+                                      } );
+                        REQUIRE( pulse_count == 5 * clock.pulses_per_quarter_note() );
+                    }
+                }
+            }
+        }
+    }
+}
+
+SCENARIO( "continue", "[midi_clock_base]" )
+{
+    using namespace sequencer;
+
+    GIVEN( "a started midi clock" )
+    {
+        midi::clock_base clock;
+        clock.start();
+        clock.update( beat_time_point( 0.0_beats ), []( midi::message_type ) {} );
+
+        WHEN( "the clock is stopped" )
+        {
+            clock.stop();
+            clock.update( beat_time_point( 0.0_beats ), []( midi::message_type ) {} );
+
+            THEN( "starting the clock again sends a continue message" )
+            {
+                clock.start();
+                midi::message_type received = midi::message_type::invalid;
+                clock.update( beat_time_point( 0.0_beats ),
+                              [&received]( midi::message_type message ) {
+                                  REQUIRE( received == midi::message_type::invalid );
+                                  received = message;
+                              } );
+                REQUIRE( received == midi::message_type::realtime_continue );
+            }
+        }
+
+        WHEN( "the clock is reset" )
+        {
+            clock.reset();
+            clock.update( beat_time_point( 0.0_beats ), []( midi::message_type ) {} );
+
+            THEN( "starting the clock again sends a start message" )
+            {
+                clock.start();
+                midi::message_type received = midi::message_type::invalid;
+                clock.update( beat_time_point( 0.0_beats ),
+                              [&received]( midi::message_type message ) {
+                                  REQUIRE( received == midi::message_type::invalid );
+                                  received = message;
+                              } );
+                REQUIRE( received == midi::message_type::realtime_start );
+            }
+        }
+    }
+}
+
+SCENARIO( "pulses", "[midi_clock_base]" )
+{
+    using namespace sequencer;
+
+    GIVEN( "a started midi clock" )
+    {
+        midi::clock_base clock;
+        clock.start();
+        WHEN( "update is called " )
+        {
+            THEN( "the clock sends 24 pulses" )
+            {
+                int pulse_count = 0;
+                clock.update( beat_time_point( 1.0_beats ),
+                              [&pulse_count]( midi::message_type message ) {
+                                  if ( message == midi::message_type::realtime_clock )
+                                  {
+                                      pulse_count++;
+                                  }
+                              } );
+                REQUIRE( pulse_count == 24 );
+                WHEN( "update is called again with duration of 1 beat" )
+                {
+                    THEN( "no pulses are send" )
+                    {
+                        pulse_count = 0;
+                        clock.update( beat_time_point( 1.0_beats ),
+                                      [&pulse_count]( midi::message_type message ) {
+                                          if ( message == midi::message_type::realtime_clock )
+                                          {
+                                              pulse_count++;
+                                          }
+                                      } );
+                        REQUIRE( pulse_count == 0 );
+                    }
+                }
+
+                WHEN( "update is called again with time point of 1 beat after reset" )
+                {
+                    clock.reset();
+                    clock.start();
+                    THEN( "24 pulses are send" )
+                    {
+                        pulse_count = 0;
+                        clock.update( beat_time_point( 1.0_beats ),
+                                      [&pulse_count]( midi::message_type message ) {
+                                          if ( message == midi::message_type::realtime_clock )
+                                          {
+                                              pulse_count++;
+                                          }
+                                      } );
+                        REQUIRE( pulse_count == 24 );
+                    }
+                }
+            }
+        }
+
+        WHEN( "pulses per quarter are changed to 36" )
+        {
+            REQUIRE( clock.pulses_per_quarter_note() == 24 );
+            clock.set_pulses_per_quarter_note( 36 );
+            REQUIRE( clock.pulses_per_quarter_note() == 36 );
+            WHEN( "update is called at 1.0 beat " )
+            {
+                THEN( "the clock sends 36 pulses" )
+                {
+                    int pulse_count = 0;
+                    clock.update( beat_time_point( 1.0_beats ),
+                                  [&pulse_count]( midi::message_type message ) {
+                                      if ( message == midi::message_type::realtime_clock )
+                                      {
+                                          pulse_count++;
+                                      }
+                                  } );
+                    REQUIRE( pulse_count == clock.pulses_per_quarter_note() );
+                }
+            }
+        }
+    }
+}

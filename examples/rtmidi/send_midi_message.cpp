525451dc51104554e3c87572647350b1ec38f37f
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-07 10:28:10 +0200

    Add midi-clock using rtmidi
    Change-Id: I4d67e6cbf2058774d5a9495f5a3b6935f7d100ad

--- examples/rtmidi/send_midi_message.cpp
+++ examples/rtmidi/send_midi_message.cpp
@@ -1,3 +1,5 @@
+#include "util.hpp"
+
 #include <sequencer/midi/message_type.hpp>
 
 #include <RtMidi.h>
@@ -7,32 +9,12 @@
 #include <thread>
 #include <vector>
 
-void cout_callback( double time_delta, std::vector< unsigned char >* message, void* /*userData*/ )
-{
-    const auto nBytes = message->size();
-    for ( decltype( message->size() ) i = 0; i < nBytes; ++i )
-        std::cout << "Byte " << i << " = " << std::hex << static_cast< int >( message->at( i ) )
-                  << ", ";
-    if ( nBytes > 0 )
-        std::cout << "expired since last message = " << time_delta << "s" << std::endl;
-}
-
-template < class RtMidi >
-std::unique_ptr< RtMidi > make_unique()
-{
-    auto rtmidi = std::make_unique< RtMidi >();
-    if ( rtmidi->getPortCount() == 0 )
-    {
-        std::cout << "No ports available!\n";
-        return nullptr;
-    }
-    rtmidi->openPort( 0 );
-    return rtmidi;
-}
+using sequencer::rtmidi::cout_callback;
+using sequencer::rtmidi::make_midi_port;
 
 void receiver( std::promise< void >& receiver_is_ready, std::future< void > sender_is_done )
 {
-    auto midiin = make_unique< RtMidiIn >();
+    auto midiin = make_midi_port< RtMidiIn >();
     if ( !midiin )
     {
         return;
@@ -56,7 +38,7 @@ int main()
         } );
     receiver_is_ready.wait();
 
-    const auto midiout = make_unique< RtMidiOut >();
+    const auto midiout = make_midi_port< RtMidiOut >();
     if ( !midiout )
     {
         return 1;

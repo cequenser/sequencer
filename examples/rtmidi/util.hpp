82138226c48b38f8589a04b5b2ca43721bab7e7f
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-07 10:28:10 +0200

    Add midi-clock using rtmidi
    Change-Id: I4d67e6cbf2058774d5a9495f5a3b6935f7d100ad

---
+++ examples/rtmidi/util.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <ios>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+namespace sequencer::rtmidi
+{
+    void cout_callback( double time_delta, std::vector< unsigned char >* message,
+                        void* /*userData*/ )
+    {
+        const auto nBytes = message->size();
+        for ( decltype( message->size() ) i = 0; i < nBytes; ++i )
+            std::cout << "Byte " << i << " = " << std::hex << static_cast< int >( message->at( i ) )
+                      << ", ";
+        if ( nBytes > 0 )
+            std::cout << "expired since last message = " << time_delta << "s" << std::endl;
+    }
+
+    template < class RtMidi >
+    std::unique_ptr< RtMidi > make_unique()
+    {
+        auto rtmidi = std::make_unique< RtMidi >();
+        if ( rtmidi->getPortCount() == 0 )
+        {
+            std::cout << "No ports available!\n";
+            return nullptr;
+        }
+        rtmidi->openPort( 0 );
+        return rtmidi;
+    }
+} // namespace sequencer::rtmidi

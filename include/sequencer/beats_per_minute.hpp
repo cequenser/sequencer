6df85484c9f664b2ba7e985b9548da4587627b92
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-09 16:31:11 +0200

    Rename beat_tempo to beats_per_minute
    Change-Id: I15f0639c64dd7ad111a2469855e0182e8d1ce4d2

---
+++ include/sequencer/beats_per_minute.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace sequencer
+{
+
+    class beats_per_minute
+    {
+    public:
+        using rep = double;
+
+        constexpr explicit beats_per_minute( rep beats_per_minute ) noexcept
+            : beats_per_minute_( beats_per_minute )
+        {
+        }
+
+        constexpr rep to_double() const noexcept
+        {
+            return beats_per_minute_;
+        }
+
+    private:
+        rep beats_per_minute_;
+    };
+
+    constexpr beats_per_minute operator"" _bpm( long double value ) noexcept
+    {
+        return beats_per_minute( value );
+    }
+
+    static_assert( ( 120.0_bpm ).to_double() == 120.0 );
+
+} // namespace sequencer

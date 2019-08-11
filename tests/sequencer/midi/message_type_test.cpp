3525360c0a26f7c021201f49bf29916ccbef3033
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-11 10:43:45 +0200

    Add song position pointer MIDI message
    Change-Id: I6c82ed7a6d4a9c656a75a6852b42b402573737a8

--- tests/sequencer/midi/message_type_test.cpp
+++ tests/sequencer/midi/message_type_test.cpp
@@ -4,7 +4,7 @@
 
 #include <sstream>
 
-SCENARIO( "message types to string", "[message_type]" )
+SCENARIO( "real time messages to string", "[message_type]" )
 {
     using namespace sequencer;
 
@@ -90,6 +90,85 @@ SCENARIO( "message types to string", "[message_type]" )
             std::stringstream stream;
             stream << message;
             REQUIRE( stream.str() == "invalid" );
+        }
+    }
+}
+
+SCENARIO( "system common messages to string", "[message_type]" )
+{
+    using namespace sequencer;
+
+    GIVEN( "song pointer position message with value 3" )
+    {
+        const auto message = midi::system::common::song_position_pointer( 3 );
+
+        THEN( "message contains 3 bytes" )
+        {
+            REQUIRE( message.size() == 3 );
+        }
+
+        THEN( "first byte is 0xF2" )
+        {
+            REQUIRE( message[ 0 ] == std::byte{0xF2} );
+        }
+
+        THEN( "second byte is 0x03" )
+        {
+            REQUIRE( message[ 1 ] == std::byte{0x03} );
+        }
+
+        THEN( "third byte is 0x00" )
+        {
+            REQUIRE( message[ 2 ] == std::byte{0x00} );
+        }
+
+        THEN( "to_string returns 'spp@3'" )
+        {
+            REQUIRE( to_string( message ) == "spp@3" );
+        }
+
+        THEN( "stream operator writes 'spp@3'" )
+        {
+            std::stringstream stream;
+            stream << message;
+            REQUIRE( stream.str() == "spp@3" );
+        }
+    }
+
+    GIVEN( "song pointer position message with value 200" )
+    {
+        const auto message = midi::system::common::song_position_pointer( 200 );
+
+        THEN( "message contains 3 bytes" )
+        {
+            REQUIRE( message.size() == 3 );
+        }
+
+        THEN( "first byte is 0xF2" )
+        {
+            REQUIRE( message[ 0 ] == std::byte{0xF2} );
+        }
+
+        THEN( "second byte is 0x48" )
+        {
+            REQUIRE( message[ 1 ] == std::byte{0x48} );
+        }
+
+        THEN( "third byte is 0x01" )
+        {
+            REQUIRE( message[ 2 ] == std::byte{0x01} );
+        }
+
+        THEN( "to_string returns 'spp@200'" )
+        {
+            REQUIRE( to_string( message ) == "spp@200" );
+        }
+
+        THEN( "stream operator writes 'spp@200'" )
+        {
+            std::stringstream stream;
+            stream << message;
+            REQUIRE( stream.str() == "spp@200" );
         }
     }
 }

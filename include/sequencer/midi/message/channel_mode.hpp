02711f9f5abada39c2e9f70f436efe3ed70c99ba
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-15 18:05:20 +0200

    Add noexcept
    Change-Id: If14c25a9c535c3090576780d4ebdca55b4de9653

--- include/sequencer/midi/message/channel_mode.hpp
+++ include/sequencer/midi/message/channel_mode.hpp
@@ -11,49 +11,49 @@ namespace sequencer::midi::channel::mode
     using control_change::status_byte;
 
     constexpr std::array< std::byte, 3 > make_message( std::byte status, std::byte control_function,
-                                                       std::byte data = std::byte{0x00} )
+                                                       std::byte data = std::byte{0x00} ) noexcept
     {
         return {status, control_function, data};
     }
 
-    constexpr std::array< std::byte, 3 > all_sounds_off( std::uint8_t channel )
+    constexpr std::array< std::byte, 3 > all_sounds_off( std::uint8_t channel ) noexcept
     {
         assert( channel < 16 );
         return make_message( status_byte( channel ), std::byte{0x78} );
     }
 
-    constexpr std::array< std::byte, 3 > reset_all_controllers( std::uint8_t channel )
+    constexpr std::array< std::byte, 3 > reset_all_controllers( std::uint8_t channel ) noexcept
     {
         assert( channel < 16 );
         return make_message( status_byte( channel ), std::byte{0x79} );
     }
 
-    constexpr std::array< std::byte, 3 > local_control( std::uint8_t channel, bool on )
+    constexpr std::array< std::byte, 3 > local_control( std::uint8_t channel, bool on ) noexcept
     {
         assert( channel < 16 );
         const auto data_byte = on ? std::byte{0x7F} : std::byte{0x00};
         return make_message( status_byte( channel ), std::byte{0x7A}, data_byte );
     }
 
-    constexpr std::array< std::byte, 3 > all_notes_off( std::uint8_t channel )
+    constexpr std::array< std::byte, 3 > all_notes_off( std::uint8_t channel ) noexcept
     {
         assert( channel < 16 );
         return make_message( status_byte( channel ), std::byte{0x7B} );
     }
 
-    constexpr std::array< std::byte, 3 > omni_mode_off( std::uint8_t channel )
+    constexpr std::array< std::byte, 3 > omni_mode_off( std::uint8_t channel ) noexcept
     {
         assert( channel < 16 );
         return make_message( status_byte( channel ), std::byte{0x7C} );
     }
 
-    constexpr std::array< std::byte, 3 > omni_mode_on( std::uint8_t channel )
+    constexpr std::array< std::byte, 3 > omni_mode_on( std::uint8_t channel ) noexcept
     {
         assert( channel < 16 );
         return make_message( status_byte( channel ), std::byte{0x7D} );
     }
 
-    constexpr std::array< std::byte, 3 > poly_mode_on( std::uint8_t channel )
+    constexpr std::array< std::byte, 3 > poly_mode_on( std::uint8_t channel ) noexcept
     {
         assert( channel < 16 );
         return make_message( status_byte( channel ), std::byte{0x7F} );

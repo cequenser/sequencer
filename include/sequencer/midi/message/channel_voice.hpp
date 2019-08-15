c4cebb2aa0a39a231596d31652500465f0f5514e
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-15 18:03:39 +0200

    Simplify channel voice messages, avoid heap allocations and support constexpr
    Change-Id: I7ca1db61969258883bd8c5b67d4f274c45b9b7c1

--- include/sequencer/midi/message/channel_voice.hpp
+++ include/sequencer/midi/message/channel_voice.hpp
@@ -1,29 +1,31 @@
 #pragma once
 
-#include <sequencer/midi/message/message_type.hpp>
+#include <sequencer/midi/message/util.hpp>
 
+#include <array>
 #include <cassert>
+#include <cstddef>
 #include <cstdint>
 
 namespace sequencer::midi::channel::voice
 {
-    inline message_type note_on( std::uint8_t channel, std::uint8_t note, std::uint8_t velocity )
+    constexpr std::array< std::byte, 3 > note_on( std::uint8_t channel, std::uint8_t note,
+                                                  std::uint8_t velocity )
     {
         assert( channel < 16 );
         assert( note < 128 );
         assert( velocity < 128 );
 
-        return make_message( status_byte_for( std::byte{0x90}, channel ), std::byte{note},
-                             std::byte{velocity} );
+        return {status_byte_for( std::byte{0x90}, channel ), std::byte{note}, std::byte{velocity}};
     }
 
-    inline message_type note_off( std::uint8_t channel, std::uint8_t note, std::uint8_t velocity )
+    constexpr std::array< std::byte, 3 > note_off( std::uint8_t channel, std::uint8_t note,
+                                                   std::uint8_t velocity )
     {
         assert( channel < 16 );
         assert( note < 128 );
         assert( velocity < 128 );
 
-        return make_message( status_byte_for( std::byte{0x80}, channel ), std::byte{note},
-                             std::byte{velocity} );
+        return {status_byte_for( std::byte{0x80}, channel ), std::byte{note}, std::byte{velocity}};
     }
 } // namespace sequencer::midi::channel::voice

64dd02f27c63dcd1e390089d91d56b92a3534727
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-16 22:22:32 +0200

    Remove message to_string conversion and streaming
    Change-Id: I810c6bd34f4ed92d9ba173f09ebd0b41f0c35c87

--- include/sequencer/midi/message/realtime.hpp
+++ include/sequencer/midi/message/realtime.hpp
@@ -1,10 +1,6 @@
 #pragma once
 
-#include <sequencer/midi/message/util.hpp>
-
 #include <cstdint>
-#include <ostream>
-#include <string_view>
 
 namespace sequencer::midi::realtime
 {
@@ -16,27 +12,4 @@ namespace sequencer::midi::realtime
         realtime_continue = 0xFB,
         realtime_stop = 0XFC
     };
-
-    constexpr std::string_view to_string( message_type type ) noexcept
-    {
-        switch ( type )
-        {
-        case message_type::realtime_clock:
-            return "clock";
-        case message_type::realtime_start:
-            return "start";
-        case message_type::realtime_continue:
-            return "continue";
-        case message_type::realtime_stop:
-            return "stop";
-        default:
-            return invalid_string;
-        }
-    }
-
-    inline std::ostream& operator<<( std::ostream& stream, message_type type )
-    {
-        stream << to_string( type );
-        return stream;
-    }
 } // namespace sequencer::midi::realtime

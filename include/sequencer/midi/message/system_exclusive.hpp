f4ec4bb935c8f2a696222e01ba3bed7acebd6b3f
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-18 19:53:24 +0200

    Finish global parameter control message
    Change-Id: I67e22f0d4dcaa4b6edc93746764f04dfda9b7517

--- include/sequencer/midi/message/system_exclusive.hpp
+++ include/sequencer/midi/message/system_exclusive.hpp
@@ -75,10 +75,79 @@ namespace sequencer::midi::system::exclusive
             return device_control( manufacturer_id, 0x04, value );
         }
 
-        constexpr message_t< 8 > global_parameter_control( std::uint8_t manufacturer_id,
-                                                           std::uint16_t value ) noexcept
+        template < std::size_t... index >
+        constexpr message_t< 4 + sizeof...( index ) >
+        make_sysex( std::uint8_t manufacturer_id, const message_t< sizeof...( index ) >& data,
+                    std::index_sequence< index... > )
         {
-            return device_control( manufacturer_id, 0x05, value );
+            return system_exclusive( manufacturer_id, data[ index ]... );
+        }
+
+        template < class Id, class Value, std::size_t number_of_values,
+                   std::size_t number_of_slots = 0 >
+        constexpr message_t< 9 + ( sizeof( Id ) + sizeof( Value ) ) * number_of_values +
+                             2 * number_of_slots >
+        global_parameter_control(
+            std::uint8_t manufacturer_id,
+            const std::array< std::pair< Id, Value >, number_of_values >& id_value_pairs,
+            const std::array< std::uint16_t, number_of_slots >& slots = {} ) noexcept
+        {
+            constexpr auto pair_size = sizeof( Id ) + sizeof( Value );
+            constexpr auto user_data_size = pair_size * number_of_values + 2 * number_of_slots;
+            message_t< 5 + user_data_size > data;
+            data[ 0 ] = device_control_byte;
+            data[ 1 ] = std::byte{0x05}; // global parameter control
+            data[ 2 ] = std::byte{number_of_slots};
+            data[ 3 ] = std::byte{sizeof( Id )};
+            data[ 4 ] = std::byte{sizeof( Value )};
+
+            for ( auto i = std::size_t{0}; i < number_of_slots; ++i )
+            {
+                const auto [ lsb, msb ] = uint16_to_lsb_msb( slots[ i ] );
+                data[ 5 + 2 * i ] = std::byte{msb};
+                data[ 5 + 2 * i + 1 ] = std::byte{lsb};
+            }
+
+            const auto offset = 5 + 2 * number_of_slots;
+            for ( auto i = std::size_t{0}; i < number_of_values; ++i )
+            {
+                if constexpr ( sizeof( Id ) == 1 && sizeof( Value ) == 1 )
+                {
+                    data[ offset + pair_size * i ] = std::byte{id_value_pairs[ i ].first};
+                    data[ offset + pair_size * i + 1 ] = std::byte{id_value_pairs[ i ].second};
+                }
+
+                if constexpr ( sizeof( Id ) == 2 && sizeof( Value ) == 1 )
+                {
+                    const auto [ id_lsb, id_msb ] = uint16_to_lsb_msb( id_value_pairs[ i ].first );
+                    data[ offset + pair_size * i ] = std::byte{id_msb};
+                    data[ offset + pair_size * i + 1 ] = std::byte{id_lsb};
+                    data[ offset + pair_size * i + 2 ] = std::byte{id_value_pairs[ i ].second};
+                }
+
+                if constexpr ( sizeof( Id ) == 1 && sizeof( Value ) == 2 )
+                {
+                    const auto [ value_lsb, value_msb ] =
+                        uint16_to_lsb_msb( id_value_pairs[ i ].second );
+                    data[ offset + pair_size * i ] = std::byte{id_value_pairs[ i ].first};
+                    data[ offset + pair_size * i + 1 ] = std::byte{value_lsb};
+                    data[ offset + pair_size * i + 2 ] = std::byte{value_msb};
+                }
+
+                if constexpr ( sizeof( Id ) == 2 && sizeof( Value ) == 2 )
+                {
+                    const auto [ id_lsb, id_msb ] = uint16_to_lsb_msb( id_value_pairs[ i ].first );
+                    data[ offset + pair_size * i ] = std::byte{id_msb};
+                    data[ offset + pair_size * i + 1 ] = std::byte{id_lsb};
+                    const auto [ value_lsb, value_msb ] =
+                        uint16_to_lsb_msb( id_value_pairs[ i ].second );
+                    data[ offset + pair_size * i + 2 ] = std::byte{value_lsb};
+                    data[ offset + pair_size * i + 3 ] = std::byte{value_msb};
+                }
+            }
+
+            return make_sysex( manufacturer_id, data,
+                               std::make_index_sequence< 5 + user_data_size >() );
         }
     } // namespace realtime
 

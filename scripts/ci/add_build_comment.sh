42630a81ab99686d4d040e1298daae7f7af306f9
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-19 16:07:23 +0200

    Trigger ci build failure
    Change-Id: I3fb58f27c4426f33ccaba03c8050ba0d58bc98f5

--- scripts/ci/add_build_comment.sh
+++ scripts/ci/add_build_comment.sh
@@ -6,8 +6,7 @@ COMPILER=$3
 
 python2.7 filter_build_output.py $LOG_FILE short_build.log
 
-sed 's/"/\\\\"/g' short_build.log | sed 's@build/../include@include@g' | sed 's@build/../tests@tests@g' | sed "s@^../include@include@g" | sed "s@ ../include@ include@g" | sed "s@^../tests@tests@g" | sed "s@ ../tests@ tests@g" > tmp_comments.log 2>&1
-
-python2.7 create_build_comment.py tmp_comments.log $BUILD_URL $COMPILER > tmp_gerrit_comments.log 2>&1
+sed 's/"/\\"/g' short_build.log | sed 's@build/../include@include@g' | sed 's@build/../tests@tests@g' | sed "s@^../include@include@g" | sed "s@ ../include@ include@g" | sed "s@^../tests@tests@g" | sed "s@ ../tests@ tests@g" > tmp_comments.log 2>&1
+create_build_comment.py tmp_comments.log $BUILD_URL $COMPILER > tmp_gerrit_comments2.log 2>&1
 
 sed -i "s/<COMMENTS>/$(sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' tmp_gerrit_comments.log | tr -d '\n')/" tests.json

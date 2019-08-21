5bd9c8e4885d67d5502b936644904720f850316f
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-21 19:15:56 +0200

    Add recording and playback example for portaudio
    Change-Id: I8150091a8d64eefc1a31ce745d09b4090fda8403

--- scripts/ci/add_clang_tidy_comments.sh
+++ scripts/ci/add_clang_tidy_comments.sh
@@ -4,7 +4,7 @@ LOG_FILE=$1
 BUILD_URL=$2
 WORK_DIR=$3
 
-sed -n '/error:/,/clang-tidy-/ { /clang-tidy-/ !p }'$LOG_FILE | sed 's/"/\\\\"/g' | sed 's@build/../include@include@g' | sed 's@build/../tests@tests@g' | sed "s@^../include@include@g" | sed "s@^include@${WORK_DIR}include@g" | sed "s@^tests@${WORK_DIR}tests@g" > tmp_comments.log 2>&1
+sed -n '/error:/,/clang-tidy-/ { /clang-tidy-/ !p }' $LOG_FILE | sed 's/"/\\\\"/g' | sed 's@build/../include@include@g' | sed 's@build/../tests@tests@g' | sed "s@^../include@include@g" | sed "s@^include@${WORK_DIR}include@g" | sed "s@^tests@${WORK_DIR}tests@g" > tmp_comments.log 2>&1
 
 python2.7 create_clang_tidy_comments.py tmp_comments.log clang-tidy $BUILD_URL $WORK_DIR > tmp_gerrit_comments.log 2>&1
 

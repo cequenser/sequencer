149e4e120153cb3cf08444bc65f65ba83412be7a
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-21 18:28:04 +0200

    Add portaudio to install script
    Change-Id: Ifad3e1462f1bc58f6235106e1908187b58d379e4

--- scripts/ci/install_dependencies_linux.sh
+++ scripts/ci/install_dependencies_linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-COMPILER=$1
+CXX_COMPILER=$1
+C_COMPILER=$2
 SEQUENCER_REPO=${PWD}
 EXTERNAL="external/repo"
 
@@ -9,7 +10,16 @@ cd ${SEQUENCER_REPO}/${EXTERNAL}
 mkdir -p install/rtmidi
 mkdir -p build/rtmidi
 cd build/rtmidi
-cmake ${SEQUENCER_REPO}/${EXTERNAL}/rtmidi -D__LINUX_ALSA__=ON -DCMAKE_INSTALL_PREFIX=${SEQUENCER_REPO}/${EXTERNAL}/install/rtmidi -DCMAKE_CXX_COMPILER=${COMPILER} -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON
+cmake ${SEQUENCER_REPO}/${EXTERNAL}/rtmidi -D__LINUX_ALSA__=ON -DCMAKE_INSTALL_PREFIX=${SEQUENCER_REPO}/${EXTERNAL}/install/rtmidi -DCMAKE_CXX_COMPILER=${CXX_COMPILER} -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON
+cmake --build . --target install
+
+### install portaudio
+cd ${SEQUENCER_REPO}/${EXTERNAL}
+tar -xvzf pa_stable_v190600_20161030.tgz
+mkdir -p install/portaudio
+mkdir -p build/portaudio
+cd build/portaudio
+cmake ${SEQUENCER_REPO}/${EXTERNAL}/portaudio -DCMAKE_INSTALL_PREFIX=${SEQUENCER_REPO}/${EXTERNAL}/install/portaudio -DCMAKE_C_COMPILER=${C_COMPILER}
 cmake --build . --target install
 
 cd ${SEQUENCER_REPO}

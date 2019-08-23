b21cce7a8dd38d13de92ab3065d75c2cf1749c08
Kai Bernhard <ukupachakamaq@gmail.com>
2019-08-23 16:51:51 +0200

    Build dependencies and fixes for Windows
    Change-Id: I341b22c4665c233cdbe896699615893b34f2c8c4

--- include/sequencer/midi/message/message_type.hpp
+++ include/sequencer/midi/message/message_type.hpp
@@ -7,8 +7,13 @@
 
 namespace sequencer::midi
 {
+    template < std::size_t lhs, std::size_t rhs >
+    struct greater_than
+    {
+        static constexpr auto value = lhs > rhs;
+    };
     template < std::size_t number_of_bytes >
     using message_t =
-        std::conditional_t< std::greater<>{}( number_of_bytes, 0 ),
+        std::conditional_t< greater_than< number_of_bytes, 0 >::value,
                             std::array< std::byte, number_of_bytes >, std::vector< std::byte > >;
 }

e7fff094356c55bf0e5c14c4a9d37fb67248750e
Lars Lubkoll <lars.lubkoll@posteo.de>
2019-08-23 16:32:55 +0200

    Refactor sample-class and add recording to app
    Change-Id: I64db919c14f874537ae7b19b88270976a21def43

--- apps/clock_ui/backend.hpp
+++ apps/clock_ui/backend.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <sequencer/midi/track.hpp>
+#include <sequencer/portaudio/portaudio.hpp>
 #include <sequencer/rtmidi/util.hpp>
 
 #include <QObject>
 #include <QString>
 #include <RtMidi.h>
+#include <chrono>
 #include <future>
+#include <mutex>
 #include <vector>
 
 namespace qml
@@ -34,7 +37,11 @@ namespace qml
 
         Q_INVOKABLE void set_song_position_pointer( int song_position_in_16th_notes );
 
-        Q_INVOKABLE QString available_ports();
+        Q_INVOKABLE QString available_midi_ports();
+
+        Q_INVOKABLE QString available_audio_devices();
+
+        Q_INVOKABLE void set_audio_device( int id );
 
         Q_INVOKABLE bool open_port( unsigned id );
 
@@ -54,12 +61,35 @@ namespace qml
 
         Q_INVOKABLE void set_note_of_current_track( int note );
 
+        Q_INVOKABLE void start_recording();
+
+        Q_INVOKABLE void stop_recording();
+
+        Q_INVOKABLE void playback();
+
+        Q_INVOKABLE bool is_recording() const noexcept;
+
     private:
+        constexpr bool no_device_selected() const noexcept
+        {
+            return audio_device_id_ == 0;
+        }
+
         RtMidiOut midiout_;
         tracks_t tracks_;
         decltype( sequencer::rtmidi::make_clock() ) clock_;
         std::future< void > clock_done_;
         std::uint8_t current_track_{0};
         std::array< sequencer::midi::note_t, number_of_tracks > track_notes_;
+        sequencer::portaudio::portaudio portaudio_{};
+        int audio_device_id_{0};
+        sequencer::portaudio::stream_t stream_;
+        std::atomic_bool stop_recording_{false};
+        std::future< void > recording_done_;
+        std::chrono::seconds max_recording_time_{3};
+        int sample_rate_ = 44100;
+        std::size_t frames_per_buffer_ = 512;
+        sequencer::audio::read_write_lockable< sequencer::audio::sample_t > sample_{
+            static_cast< std::size_t >( max_recording_time_.count() ) * sample_rate_};
     };
 } // namespace qml

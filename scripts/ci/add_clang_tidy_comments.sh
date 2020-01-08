fbab07515af8e56637a7f4a31d19969e4afd602f
Lars Lubkoll <lars.lubkoll@posteo.de>
2020-01-08 20:10:37 +0100

    tst clang-tidy
    Change-Id: I77784c6c1241e4a81e9209ba08ca61a20f5539d1

--- scripts/ci/add_clang_tidy_comments.sh
+++ scripts/ci/add_clang_tidy_comments.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-LOG_FILE=$1
-BUILD_URL=$2
-WORK_DIR=$3
+FIXES=${1}
+BUILD_URL=${2}
 
-sed -n '/error:/,/clang-tidy-/ { /clang-tidy-/ !p }' $LOG_FILE | sed 's/"/\\\\"/g' | sed 's@build/../include@include@g' | sed 's@build/../tests@tests@g' | sed "s@^../include@include@g" | sed "s@^include@${WORK_DIR}include@g" | sed "s@^tests@${WORK_DIR}tests@g" > tmp_comments.log 2>&1
+sed 's@build/../include@include@g' ${FIXES} | sed 's@build/../tests@tests@g' | sed "s@^../include@include@g" > adjusted_fixes.yml 2>&1
 
-python2.7 create_clang_tidy_comments.py tmp_comments.log clang-tidy $BUILD_URL $WORK_DIR > tmp_gerrit_comments.log 2>&1
-
+python2.7 scripts/ci/create_clang_tidy_comments.py adjusted_fixes.yml ${BUILD_URL} > tmp_gerrit_comments.log
 sed -i "s/<COMMENTS>/$(sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' tmp_gerrit_comments.log | tr -d '\n')/" clang-tidy.json
+

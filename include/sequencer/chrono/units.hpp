930bb3de2edb255fb2f567e9b9dd743ae6333f11
Lars Lubkoll <lars.lubkoll@posteo.de>
2020-01-08 20:10:37 +0100

    tst clang-tidy
    Change-Id: I77784c6c1241e4a81e9209ba08ca61a20f5539d1

--- include/sequencer/chrono/units.hpp
+++ include/sequencer/chrono/units.hpp
@@ -2,9 +2,15 @@
 
 #include <chrono>
 
+
 namespace sequencer::chrono
 {
-    using milliseconds = std::chrono::duration< double, std::milli >;
+
+using milliseconds = std::chrono::duration< double, std::milli >;
     using seconds = std::chrono::duration< double, std::ratio< 1, 1 > >;
-    using minutes = std::chrono::duration< double, std::ratio< 60, 1 > >;
+      using minutes = std::chrono::duration< double, std::ratio< 60, 1 > >;
+    using TTTT = std::chrono::duration< double, std::ratio< 60, 1 > >;
+    const auto sssss = 0;
+    const auto tttt =
+            reinterpret_cast< const void* >( &sssss );
 } // namespace sequencer::chrono

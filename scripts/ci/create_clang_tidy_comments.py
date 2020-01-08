62b1d094a252228e9da7ae0a50dbeeff3a9c8763
Lars Lubkoll <lars.lubkoll@posteo.de>
2020-01-08 20:10:37 +0100

    tst clang-tidy
    Change-Id: I77784c6c1241e4a81e9209ba08ca61a20f5539d1

--- scripts/ci/create_clang_tidy_comments.py
+++ scripts/ci/create_clang_tidy_comments.py
@@ -3,112 +3,100 @@
 import os
 import re
 import sys
+from copy import deepcopy
 
 indent = '  '
 
 
-def get_message_start_index(line):
-    message_start = line.find('error:') + 7
-    if message_start == 6:
-        message_start = line.find('note:')
-    return message_start
+DIAGNOSTIC_NAME = 'DiagnosticName'
+FILE_OFFSET = 'FileOffset'
+FILE_PATH = 'FilePath'
+MESSAGE = 'Message'
+REPLACEMENTS = 'Replacements'
+LENGTH = 'Length'
+OFFSET = 'Offset'
+REPLACEMENT_TEXT = 'ReplacementText'
 
 
-def get_filename_and_linenumber(line, base_path = None):
-    if base_path:
-        m = re.match(r'^' + base_path + r'(\S*):([0-9]+):[0-9]+:', line)
-    else:
-        m = re.match(r'^(\S+):([0-9]+):[0-9]+:', line)
-    if m:
-         return m.groups()[0], m.groups()[1]
-    return None, None    
+class HashableDict(dict):
+    def __hash__(self):
+        return hash(tuple(sorted(self.items())))
 
 
-def line_filter(line):
-    return not (line in ['\n', '\r\n'] or
-                "warnings generated" in line or
-                "warnings treated as errors" in line or
-                line.startswith("Suppressed") or
-                line.startswith("Use -header-filter=.*"))
-
-
-def get_error_message(clang_file, line, filename):
-    message = line[get_message_start_index(line):]
-
-    line = clang_file.readline()
-    while line:
-        if line_filter(line):
-            message += line
+def read_replacements(line, clang_file):
+    while REPLACEMENT_TEXT not in line:
         line = clang_file.readline()
-        other_filename, other_linenumber = get_filename_and_linenumber(line)
-        if other_filename and other_filename != filename:
+    return line[line.find(REPLACEMENT_TEXT) + len(REPLACEMENT_TEXT) + 2:line.find('}')], line
+
+
+def read_diagnostics(clang_filename):
+    clang_file = open(clang_filename, 'r')
+    diagnostics = {}
+    clang_file.readline()
+    while True:
+        line = clang_file.readline()
+        if not line.startswith('- ' + DIAGNOSTIC_NAME):
             break
-
-    return message, line
-
-
-def add_notes(clang_file, line, filename, base_path, message):
-    while line:
-        other_filename, other_linenumber = get_filename_and_linenumber(line, base_path)
-        if other_filename:
-            if other_filename == filename and 'note:' in line:
-                message += '\n' + line[get_message_start_index(line):]
-	    elif 'error:' in line:
-                break
+        diagnostic = HashableDict()
+        diagnostic[DIAGNOSTIC_NAME] = line[len(DIAGNOSTIC_NAME) + 4:-1]
         line = clang_file.readline()
-
-    return message, line
-
-
-def read_message(clang_file, line, base_path):
-    if not line.startswith(base_path):
+        diagnostic[FILE_OFFSET] = int(line[len(FILE_OFFSET) + 4:-1])
         line = clang_file.readline()
-        return line, None, None
-
-    filename, linenumber = get_filename_and_linenumber(line, base_path)
-    message, line = get_error_message(clang_file, line, filename)
-    message, line = add_notes(clang_file, line, filename, base_path, message)
-
-    if not '^' in message:
-        return line, None, None
-    return line, filename, (linenumber, message)
-
-
-def extract_filename_dictionary(filename, base_path):
-    clang_file = open(filename, 'r')
-    comments = {}
-
-    line = clang_file.readline()
-    while line:
-        line, filename, message = read_message(clang_file, line, base_path)
-        if filename:
-            if filename in comments.keys():
-                comments[filename].append(message)
-            else:
-                comments[filename] = [message]
-
-    for key in comments.keys():
-        comments[key] = list(set(tuple(comments[key])))
-    return comments
+        file_path = line[len(FILE_PATH) + 4:-1]
+        line = clang_file.readline()
+        diagnostic[MESSAGE] = line[len(MESSAGE) + 4:-1]
+        line = clang_file.readline()
+        if REPLACEMENTS + ': []' in line:
+            diagnostic[REPLACEMENT_TEXT] = None
+        else:
+            line = clang_file.readline()
+            diagnostic[REPLACEMENT_TEXT], line = read_replacements(line, clang_file)
+        if not diagnostics.get(file_path):
+            diagnostics[file_path] = set()
+        diagnostics[file_path].add(diagnostic)
+    return diagnostics
 
 
-def create_comment_section(comments, robot_id, robot_run_id):
+def get_message(diagnostic, line, offset):
+    indent = ' ' * offset
+    message = 'error: ' + diagnostic[DIAGNOSTIC_NAME] + '\n' + line + indent + '^'
+    if diagnostic.get(REPLACEMENT_TEXT):
+        message += ('~' * (len(diagnostic[REPLACEMENT_TEXT]) - 1)) + '\n'
+        message += indent + diagnostic[REPLACEMENT_TEXT]
+    return message
+
+
+def get_line_and_message(diagnostic, filename):
+    file_ = open(filename, 'r')
+    offset = deepcopy(diagnostic[FILE_OFFSET])
+    line_number = -1
+    for line in file_:
+        line_number += 1
+        if offset - len(line) < 0:
+            return line_number, get_message(diagnostic, line, offset)
+        offset -= len(line)
+    return None, None
+
+
+def write_comments(diagnostics, robot_id, robot_run_id):
     comment_section = ""
     first_filename = True
-    for filename, comment in comments.items():
+    for filename, diagnostic_set in diagnostics.items():
         if not first_filename:
             comment_section = comment_section + ',\n'
         first_filename = False
         comment_section = comment_section + '"' + filename + '": [\n'
         first_comment = True
-        for line_number, message in comment:
+
+        for diagnostic in diagnostic_set:
+            line_number, message = get_line_and_message(diagnostic, filename)
             if not first_comment:
                 comment_section = comment_section + ',\n'
             first_comment = False
             start_comment = indent*3 +'{\n'
             comment_body = indent*4 + '"robot_id": "' + robot_id + '",\n'
             comment_body = comment_body + indent*4 + '"robot_run_id": "' + robot_run_id + '",\n'
-            comment_body = comment_body + indent*4 + '"line": "' + line_number + '",\n'
+            comment_body = comment_body + indent*4 + '"line": "' + str(line_number) + '",\n'
             comment_body = comment_body + indent*4 + '"message": "' + message + '"\n'
             end_comment = indent*3 + '}'
             comment_section = comment_section + start_comment + comment_body + end_comment
@@ -117,11 +105,11 @@ def create_comment_section(comments, robot_id, robot_run_id):
     return comment_section
 
 
-def create_clang_tidy_comments(clang_filename, robot_id, robot_run_id, base_path):
-    comments = extract_filename_dictionary(clang_filename, base_path)
-    comment_section = create_comment_section(comments, robot_id, robot_run_id)
-    print comment_section
+def create_clang_tidy_comments(clang_filename, robot_run_id):
+    diagnostics = read_diagnostics(clang_filename)
+    comments = write_comments(diagnostics=diagnostics, robot_id='clang-tidy', robot_run_id=robot_run_id)
+    print comments
 
 
-if len(sys.argv) == 5:
-    create_clang_tidy_comments(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+if len(sys.argv) == 3:
+    create_clang_tidy_comments(sys.argv[1], sys.argv[2])
